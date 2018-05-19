424930b84b16fabe7c906470bca40133e8bb663b
Ricardo Ordoñez <richard11_71@outlook.com>
2018-05-19 18:11:13 -0500

    display Search component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header';
+import Search from './components/Search/Search';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Header />
+        <Search />
       </React.Fragment>
     );
   }
