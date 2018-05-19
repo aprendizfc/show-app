ae6230f95a17e886b3e0ca709bcbec2673b44f82
Ricardo Ordoñez <richard11_71@outlook.com>
2018-05-19 16:30:36 -0500

    display the header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Header from './components/Header/Header';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Show app
-      </div>
+      <React.Fragment>
+        <Header />
+      </React.Fragment>
     );
   }
 }
