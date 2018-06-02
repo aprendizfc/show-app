d2dfe7f8e2f7ebf601c330210b6fbbae4f4a59df
Ricardo Ordoñez <richard11_71@outlook.com>
2018-06-02 15:49:09 -0500

    fetch movies

--- src/screens/Movies/Movies.js
+++ src/screens/Movies/Movies.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
+import api from '../../services/api';
 
 class Movies extends Component {
+  componentDidMount() {
+    const headers = {
+      method: 'GET',
+    }
+    api.get('movie/popular', headers)
+      .then((res) => res.json())
+      .then(data => {
+        console.log('​Movies -> componentDidMount -> data', data);
+      })
+      .catch(err => {
+        console.log('​Movies -> componentDidMount -> err', err);
+      })
+  }
   render() {
     return (
       <h1>Movies</h1>
