b0914ebaf71071bc8da5b79b8f4a12973279081c
Ricardo Ordoñez <richard11_71@outlook.com>
2018-06-02 14:48:52 -0500

    render apropiate component for each route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect
+} from 'react-router-dom';
+
+// components
 import Header from './components/Header/Header';
 import Search from './components/Search/Search';
+
+// screens
+import ScreenMovies from './screens/Movies/Movies';
+import ScreenSeries from './screens/Series/Series';
+import ScreenFavorites from './screens/Favorites/Favorites';
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Header />
-        <Search />
-      </React.Fragment>
+      <Router>
+        <React.Fragment>
+          <Header />
+          <Search />
+
+          <section className="container">
+            <Route exact path="/" render={() => <Redirect to="/movies" />} />
+            <Route path="/movies" component={ScreenMovies} />
+            <Route path="/series" component={ScreenSeries} />
+            <Route path="/favorites" component={ScreenFavorites} />
+          </section>
+        </React.Fragment>
+      </Router>
     );
   }
 }
