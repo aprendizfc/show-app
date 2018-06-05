6ab5a03c3df312a53b2db5a9e9b6500f34b3e828
Ricardo Ordoñez <richard11_71@outlook.com>
2018-06-04 21:58:19 -0500

    fetch tv shows

--- src/screens/Series/Series.js
+++ src/screens/Series/Series.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
+import api from '../../services/api';
 
 class Series extends Component {
+  state = {
+    result: [],
+  }
+
+  componentDidMount() {
+    const headers = {
+      method: 'GET',
+    }
+
+    api.get('tv/popular', headers)
+      .then(res => res.json())
+      .then(data => {
+        console.log('​Series -> componentDidMount -> data', data);
+        this.setState({
+          result: data.results,
+        })
+      })
+      .catch(err => {
+        console.log('​Series -> componentDidMount -> err', err);
+      })
+  }
   render() {
     return (
       <h1>Series</h1>
