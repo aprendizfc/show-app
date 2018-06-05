82520370366bc0067bb0229d0300c5d45922fb62
Ricardo Ordoñez <richard11_71@outlook.com>
2018-06-04 21:48:07 -0500

    create a component to display an item

---
+++ src/components/DisplayList/DisplayList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// assets
+import styles from './DisplayList.css';
+
+const Movie = function(props) {
+  const { movie } = props;
+  const POSTER_BASE_PATH = 'https://image.tmdb.org/t/p/w370_and_h556_bestv2'
+  const POSTER_PATH = movie.poster_path;
+
+  return (
+    <div className={`card flex ${styles.posterCard}`}>
+      <div
+        className={styles.posterHolder}
+        style={{backgroundImage: `url(${POSTER_BASE_PATH}${POSTER_PATH})`}}
+      >
+      </div>
+      <div className={styles.posterDetail}>
+        <h4 className={`flex between ${styles.posterTitle}`}>
+          {movie.title}
+          <span className={styles.posterAverage}>{movie.vote_average}</span>
+        </h4>
+        <p className={styles.posterOverview}>
+          {movie.overview}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  movie: PropTypes.object.isRequired,
+}
+
+export default Movie;
