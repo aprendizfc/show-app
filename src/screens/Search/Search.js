dab35481ef8aea17b7d120782adb6f6d75cb6025
Ricardo Ordoñez <richard11_71@outlook.com>
2018-07-09 23:09:27 -0500

    create Search screen

---
+++ src/screens/Search/Search.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+
+class Search extends Component {
+  render() {
+    return (
+      <h1>Search result</h1>
+    );
+  }
+}
+
+export default Search ;
