6659fe7f9cc98ef76193cfd959936426146b5af2
Ricardo Ordoñez <richard11_71@outlook.com>
2018-07-09 23:11:08 -0500

    create search result route

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ import {
 
 // components
 import Header from './components/Header/Header';
-import Search from './components/Search/Search';
+import SearchForm from './components/SearchForm/SearchForm';
 
 // screens
 import ScreenMovies from './screens/Movies/Movies';
 import ScreenSeries from './screens/Series/Series';
 import ScreenFavorites from './screens/Favorites/Favorites';
+import ScreenSearch from './screens/Search/Search';
 
 class App extends Component {
   render() {
@@ -20,13 +21,14 @@ class App extends Component {
       <Router>
         <React.Fragment>
           <Header />
-          <Search />
+          <SearchForm />
 
           <section className="container flex flex-wrap">
             <Route exact path="/" render={() => <Redirect to="/movies" />} />
             <Route path="/movies" component={ScreenMovies} />
             <Route path="/series" component={ScreenSeries} />
             <Route path="/favorites" component={ScreenFavorites} />
+            <Route path="/search/:query" component={ScreenSearch} />
           </section>
         </React.Fragment>
       </Router>
