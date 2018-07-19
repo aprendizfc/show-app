2795ba462019337f5e1fb95d3f1024423fb3a27c
Ricardo Ordoñez <richard11_71@outlook.com>
2018-07-18 21:58:14 -0500

    add functionality to get favorites

--- src/screens/Favorites/Favorites.js
+++ src/screens/Favorites/Favorites.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 
+// components
+import FavoritesComponent from '../../components/Favorites/Favorites';
+
 class Favorites extends Component {
+  state = {
+    favorites: [],
+  }
+
+  componentDidMount() {
+    const favorites = this.getFavorites();
+    this.setState({
+      favorites,
+    })
+  }
+
+  getFavorites() {
+    return JSON.parse(localStorage.getItem("favorites"));
+  }
+
   render() {
     return (
-      <h1>Favorites</h1>
+      <FavoritesComponent
+        list={this.state.favorites}
+      />
     );
   }
 }
